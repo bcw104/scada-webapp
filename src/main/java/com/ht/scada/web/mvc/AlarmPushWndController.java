0fd9dd8f3ffef5d44d31a0830456f7a296a6529c
侯 <30905590@qq.com>
2013-06-13 08:58:23 +0800

    删除没有用的引用

--- src/main/java/com/ht/scada/web/mvc/AlarmPushWndController.java
+++ src/main/java/com/ht/scada/web/mvc/AlarmPushWndController.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ht.scada.data.RealtimeDataMessageDelegate;
 import com.ht.scada.data.RealtimeMessageListener;
-import com.ht.scada.data.entity.FaultRecord;
 import com.ht.scada.data.service.AlarmService;
 import com.ht.scada.data.service.testimpl.TestDataDao;
 import com.ht.scada.security.service.UserService;
@@ -15,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mvc.atmosphere.AtmosphereUtils;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,11 +22,9 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.inject.Inject;
 import java.io.IOException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
